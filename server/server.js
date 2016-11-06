dfc584090445eaf217d3854fe6b765f175272d2a
Paul McClure <paulage@gmail.com>
2016-11-06 15:43:02 -0800

    test DELETE todos

--- server/server.js
+++ server/server.js
@@ -48,15 +48,35 @@ app.get('/todos/:id', (req, res) => {
 
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            return res.status(404).send();          
+            return res.status(404).send();
         }
 
         res.send({ todo });
-
     }).catch((e) => {
         res.status(400).send();
     });
 });
+
+
+//DELETE /todos:id
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});      
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`running on port ${port}`)
